11ab413b381df3cfea85a30a6314d6600a5e73e4
JJLorente <joel.jimenez.lorente@gmail.com>
2020-04-30 20:29:26 +0200

    setOne customer

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -10,5 +10,29 @@ module.exports = {
     } catch (error) {
       return res.status(500).send("Error find userId").end();
     }
+  },
+
+  setOne: async (req, res) => {
+    try {
+      const userId = req.user.id ? req.user.id : null;
+      if(!userId) return res.status(400).send("userId required");
+      const username = req.body.username ? req.body.username : null;
+      const description = req.body.description ? req.body.description : null;
+      const address = req.body.address ? req.body.address : null;
+      const location = req.body.location ? req.body.location : null;
+      const picture = req.body.picture ? req.body.picture : null;
+
+      const userUpdated = {}
+      if (username) userUpdated.name = username;
+      if (description) userUpdated.description = description;
+      if (address && address.length) userUpdated.full_address = address;
+      if (location && location.lng && location.lat) userUpdated.location = { type: "Point", coordinates: [location.lng, location.lat] };
+      if (picture) userUpdated.picture = picture;
+      const user = await customerModel.updateOne({"_id": userId}, userUpdated);
+      
+      return res.json(user).end();
+    } catch (error) {
+      return res.status(500).send("Error find userId").end();
+    }
   }
 }
