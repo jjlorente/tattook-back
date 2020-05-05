91f39cd310fdb522ee3837df71a8cbcc781e4074
JJLorente <joel.jimenez.lorente@gmail.com>
2020-05-05 16:26:55 +0200

    add info customer en ruta /work

--- modules/work/work.controller.js
+++ modules/work/work.controller.js
@@ -1,10 +1,19 @@
 const workModel = require("./work.model").WorkModel
+const thumbnailModel = require("../work/thumbnail.model").ThumbnailModel
+const workPortfolioModel = require("../work/work_portfolio.model").WorkPortfolioModel
+const imageUtils = require("../../core/image-utils");
+const customerModel = require("../user/user.model").CustomerModel;
 
 module.exports = {
   getAllWorks: async (req, res) =>{
     try {
       const workList = await workModel.find({}).sort({uploadDate: -1});
-      return res.json(workList).end();
+      const customerPromisesList = workList.map(async (work) => {
+        return customerModel.find({"_id": work._id_artist});
+      });
+      let customerList = await Promise.all(customerPromisesList);
+      customerList = customerList.map(t=>t[0]);
+      return res.json(customerList).end();
     } catch (error) {
       return res.status(500).json({error: "Error en recoger imagenes"}).end();
     }
