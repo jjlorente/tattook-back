ec8ab63b71e2ce94f356a6ee8bd924578b7b1877
JJLorente <joel.jimenez.lorente@gmail.com>
2020-05-05 23:42:54 +0200

    recoger miniatura y no imagen original

--- modules/work/work.controller.js
+++ modules/work/work.controller.js
@@ -7,8 +7,30 @@ const customerModel = require("../user/user.model").CustomerModel;
 module.exports = {
   getAllWorks: async (req, res) =>{
     try {
-      const workList = await workModel.find({}).sort({uploadDate: -1});
-      return res.json(workList).end();
+      //recogemos trabajos sin la foto
+      const workList = await workModel.find({}, '-picture').sort({uploadDate: -1});
+
+      //por cada trabajo recogemos su miniatura
+      const thumbPromises = workList.map(async (work)=>{
+        return thumbnailModel.findOne({"_id_picture":work._id})
+      })
+      const thumbList = await Promise.all(thumbPromises);
+
+      //por cada trabajo recogemos su artista
+      const userPromises = workList.map(async (work)=>{
+        return customerModel.findById(work._id_artist)
+      })
+      const userList = await Promise.all(userPromises);
+
+      //pasamos por cada trabajo y unimos todos los datos en un objeto
+      const workWithUser = workList.map((work, index)=>{
+        return {
+          thumb:thumbList[index],
+          work: work,
+          user: userList[index]
+        }
+      })
+      return res.json(workWithUser).end();
     } catch (error) {
       return res.status(500).json({error: "Error en recoger imagenes"}).end();
     }
